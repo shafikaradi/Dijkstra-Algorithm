31212158e288e0a9c2219a7d7a80c11b95017e30
Shafiq Elaradi <shafik93@icloud.com>
2018-02-11 16:37:11 +0200

    my fourth commit

--- Graph.java
+++ Graph.java
@@ -1,8 +1,28 @@
 public class Graph{
 
-    Node A = new Node("A",0);
-    Node B = new Node("A",4,A);
-    Node C = new Node("A",3,A);
-    Node D = new Node("A",5,A);
+    Node a = null;
+    Node b = null;
+    Node c = null;
+    Node d = null;
+    /* First Function has to be invoked after the object being
+     instantiated to build the graph */
+
+    public Graph buildGraph(){
+
+         a = new Node("A",0);
+         b = new Node("B",4,a);
+         c = new Node("C",3,a);
+         d = new Node("D",5,a);
+    
+
+        return this;
+    }
+     
+    public void findShortestPath(){
+
+
+        
+    }
+
 
 }
